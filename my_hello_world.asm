56537cf0c364083644d42bda240c7d76d6638679
nihk <nickjrose@gmail.com>
2015-09-17 13:24:32 -0400

    First commit

---
+++ my_hello_world.asm
@@ -0,0 +1,21 @@
+
+org 100h
+ 
+jmp start
+          
+msg db "Hello", 10, 13, "$" ; create a newline
+msg2 db "World!$"  
+
+start:
+                  
+mov AH, 9                   ; print string subfunction
+mov DX, offset msg
+int 21h    
+mov DX, offset msg2
+int 21h   
+                         
+ret
+
+
+
+
