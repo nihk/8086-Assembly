af4a63a45e29cfd4ef97164ccbff7470ed9710a9
nihk <nickjrose@gmail.com>
2015-12-16 19:08:24 -0500

    Added file

---
+++ xor_xcryption.asm
@@ -0,0 +1,36 @@
+; Prints out the "cipher" string encrypted
+; then its decrypted (original) form on a new line
+
+org 100h
+
+mov AH, 0Eh
+lea BX, cipher
+
+call xcrypt                     ; encrypt
+   
+mov AL, 0Ah                     ; \n
+int 10h
+mov AL, 0Dh                     ; \r
+int 10h
+
+lea BX, cipher                  ; reload start of variable's address
+
+call xcrypt                     ; decrypt
+
+ret
+
+key EQU 37
+cipher db "abcd123", 0
+
+xcrypt PROC
+    xcryptLoop:
+        XOR [BX], key
+        mov AL, [BX]
+        int 10h
+        inc BX
+        cmp [BX], 0
+        jne loop xcryptLoop
+    ret
+xcrypt ENDP
+
+end
